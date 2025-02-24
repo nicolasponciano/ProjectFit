31d8788d6f456504ec19de54d301c2f4bb0fdc02
Nicolas leonardo <nickponciano@hotmail.com>
2025-02-24 03:23:41 -0300

    Feita tela de abou e permissão para admin na tela de gerenciamneto

---
+++ Controllers/BasePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using ProjectFit.Models;
+
+public class BasePage : Page
+{
+    // Renomeado para evitar conflito com possíveis controles
+    protected bool UserIsAdmin { get; private set; }
+    protected string CurrentUserId { get; private set; }
+
+    protected override void OnInit(EventArgs e)
+    {
+        base.OnInit(e);
+
+        if (!Context.User.Identity.IsAuthenticated)
+        {
+            Response.Redirect("~/Account/Login.aspx", false);
+            Context.ApplicationInstance.CompleteRequest();
+            return;
+        }
+
+        CurrentUserId = Context.User.Identity.GetUserId();
+
+        using (var db = new ApplicationDbContext())
+        {
+            var user = db.Users.FirstOrDefault(u => u.Id == CurrentUserId);
+            if (user != null)
+            {
+                UserIsAdmin = user.IsAdmin;
+                System.Diagnostics.Debug.WriteLine("UserIsAdmin na BasePage (OnInit): " + UserIsAdmin);
+            }
+            else
+            {
+                Response.Redirect("~/Account/Login.aspx", false);
+                Context.ApplicationInstance.CompleteRequest();
+                return;
+            }
+        }
+    }
+}
--- Views/About.aspx.designer.cs
+++ Views/About.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace ProjectFit
 
     public partial class About
     {
-
-        /// <summary>
-        /// Controle Button1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
